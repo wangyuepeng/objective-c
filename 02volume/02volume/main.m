d11cd866e3bb070ebd0613fcad692e90ae26e9fe
wang yuepeng <18841159952@163.com>
2015-01-31 11:31:47 +0800

    create
    2015-01-31 create

---
+++ 02volume/02volume/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  02volume
+//
+//  Created by 王玥鹏 on 15/1/31.
+//  Copyright (c) 2015年 wang yuepeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
