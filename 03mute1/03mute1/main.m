bb3791242ec63b513a0956546eba258c70ef62e4
wang yuepeng <18841159952@163.com>
2015-02-01 11:18:51 +0800

    1
     1

---
+++ 03mute1/03mute1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  03mute1
+//
+//  Created by 王玥鹏 on 15/2/1.
+//  Copyright (c) 2015年 wang yuepeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
