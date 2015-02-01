e2a45500410409e0f31b9851a0f92edabbf7a83e
wang yuepeng <18841159952@163.com>
2015-02-01 11:37:57 +0800

    03mute1
    03mute1

---
+++ 03mute1/03mute1/MuteVolume.h
@@ -0,0 +1,15 @@
+//
+//  MuteVolume.h
+//  03mute1
+//
+//  Created by 王玥鹏 on 15/2/1.
+//  Copyright (c) 2015年 wang yuepeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Volume.h"
+
+@interface MuteVolume : Volume
+
+-(id)mute;
+@end
---
+++ 03mute1/03mute1/MuteVolume.m
@@ -0,0 +1,17 @@
+//
+//  MuteVolume.m
+//  03mute1
+//
+//  Created by 王玥鹏 on 15/2/1.
+//  Copyright (c) 2015年 wang yuepeng. All rights reserved.
+//
+
+#import "MuteVolume.h"
+
+@implementation MuteVolume
+-(id)mute
+{
+    val = min;
+    return self;
+}
+@end
